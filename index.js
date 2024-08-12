baf34465acb9df84542e8ecc81ab976a86304ec2
NFRamz <133985045+NFRamz@users.noreply.github.com>
2024-08-12 23:09:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ fastify.get('/', (request, reply) => {
   reply.sendFile('index.html');
 });
 
-fastify.post('/tmp','/upload', {
+fastify.post('/upload', {
   preValidation: fastify.cfTurnstile,
   config: {
     rateLimit: {
@@ -71,7 +71,7 @@ fastify.post('/tmp','/upload', {
       }
 
       const randomFilename = generateRandomString(16) + path.extname(part.filename);
-      const uploadDir = path.join(__dirname, 'uploads');
+      const uploadDir = path.join('/tmp', 'uploads');
       if (!fs.existsSync(uploadDir)) {
         fs.mkdirSync(uploadDir);
       }
